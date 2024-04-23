b32847d0b6dd8e4e2139ce268046ebdc3386abcd
Rin Steitz <erin.steitz@gmail.com>
2024-04-22 23:22:44 -0400

    Update for the project draft

--- 05_Project/README.md
+++ 05_Project/README.md
@@ -1,3 +1,18 @@
-# Project
+# Final Project
+
+For my project, I wanted to merge text encoding and segmentation with data scraped from a social media platform.
+
+## Collecting the data:
+To begin, I scraped data from a particular post on Reddit and stored the text in a file named reddit_body.txt.
+
+## Cleaning the data:
+Next, using the code from encoding.py, all characters will be uniformally encoded, ensuring that there are no duplicates between similar tokens.
+
+## Segmenting the data:
+In this step, the data is segmented: each sentence is returned on a new line for text analysis.
+
+## Sentiment Analysis:
+Using VADER, sentiment analysis was performed on the text scraped from the original Reddit post.
+
 
 `PROJECT DESCRIPTION GOES HERE`
---
+++ 05_Project/reddit_data_L545.py
@@ -0,0 +1,146 @@
+import praw
+import pandas as pd
+from datetime import datetime as dt
+
+# Set up id and verify it
+reddit = praw.Reddit(client_id="9eaZgAHFqJAkFbK6fFJAMg",
+	client_secret="o1hw9hNDdt_JfYQJg0_37A2lwaQ6gA",
+	password="password",
+	user_agent="Final project to analyze public sentiment about the conflict in Israel/Palestine, username=fallenfloral")
+
+# Add an empty data variable to store all the data to be used for pandas
+attributes_dict = { "id":[],
+        "author":[],
+        "body":[],
+        "upvotes": [],
+        "created": [],
+        "parent_id": []}
+
+# Specify which submission will be analyzed
+submission = reddit.submission("1bk96ug")
+
+# Avoid the 'MoreComments' common error
+submission.comments.replace_more(limit=None)
+
+# Create a variable to store the text of the reddit post (comments and replies)
+f = open("reddit_body.txt", "w")
+
+test_c = 0
+
+test_r = 0
+
+# Iterate over the comments in the specific post to get each original comment
+for comment in submission.comments.list():
+	if comment.body in attributes_dict["body"]: continue
+	attributes_dict["id"].append(comment.id)
+	attributes_dict["author"].append(comment.author.name if comment.author else "[deleted]")
+	attributes_dict["body"].append(comment.body)
+	attributes_dict["upvotes"].append(comment.score)
+	attributes_dict["created"].append(comment.created_utc)
+	attributes_dict["parent_id"].append(comment.parent().id)  # Store the parent comment ID
+	print(comment.body, file=f)
+	test_c = test_c + 1
+
+	# Iterate through the original comments to get all the embedded comments
+	for reply in comment.replies:
+		if reply.body in attributes_dict["body"]: continue
+		attributes_dict["id"].append(reply.id)
+		attributes_dict["author"].append(reply.author.name if reply.author else "[deleted]")
+		attributes_dict["body"].append(reply.body)
+		attributes_dict["upvotes"].append(reply.score)
+		attributes_dict["created"].append(reply.created_utc)
+		attributes_dict["parent_id"].append(reply.parent().id)  # Store the parent comment ID
+		test_r = test_r + 1
+		print(reply.body, file=f)
+
+print(test_c)
+print(test_r)
+
+# Remove duplicates
+# unique_lines = []
+# with open('reddit_body.txt', 'r') as f:
+#	for line in f.readlines():
+#		if line in attributes_dict: continue
+#		attributes_dict.append(line)
+#with open('reddit_body_clean.txt', 'w') as f:
+#	f.writelines(attributes_dict)
+
+# Create a dataframe
+attributes_data = pd.DataFrame(attributes_dict)
+
+def get_date(created):
+	#return dt.fromtimestamp(created)
+	return dt.fromtimestamp(created)
+
+attributes_data["timestamp"] = attributes_data["created"].apply(get_date)
+
+print(attributes_data)
+
+# Close the file
+f.close()
+
+# Export the data to a csv file for visualization
+# cip stands for comments in post
+attributes_data.to_csv('cip_PRAWreddit.csv', index=False)
+
+
+
+
+
+
+
+
+'''
+OLD code. Picked and chose from this. Will clean it up later.
+
+import praw
+import pandas as pd
+from datetime import datetime as dt
+from praw.models import MoreComments
+
+
+reddit = praw.Reddit(
+        client_id="9eaZgAHFqJAkFbK6fFJAMg", 
+        client_secret="o1hw9hNDdt_JfYQJg0_37A2lwaQ6gA", 
+        password="password",
+        user_agent="Project to analyze public sentiment about the Israel/Palestine"
+	)
+print(reddit)
+
+posts = []
+# Add an empty data variable to store all the data to be used for pandas
+attributes_dict = { "title":[],
+                "score":[],
+                "id":[], "url":[],
+                "comms_num": [],
+                "created": [],
+                "body":[]}
+
+subreddit = reddit.subreddit('r/AmericanPolitics')
+print(subreddit)
+submission = reddit.submission("1c019n5")
+# Avoid the 'MoreComments' common error
+#submission.comments.replace_more(limit=None)
+print(submission.comments)
+# Iterate over the comments in the specific post to get each one
+for comment in submission.comments.list():
+        attributes_dict["title"].append(submission.title)
+        attributes_dict["score"].append(submission.score)
+        attributes_dict["id"].append(submission.id)
+        attributes_dict["url"].append(submission.url)
+        attributes_dict["comms_num"].append(submission.num_comments)
+        attributes_dict["created"].append(submission.created)
+        attributes_dict["body"].append(submission.selftext)
+        posts.append(attributes_dict)
+        attributes_data = pd.DataFrame(attributes_dict)
+        print("up to here")
+        print(comment.body)
+
+attributes_data = pd.DataFrame(attributes_dict)
+
+def get_date(created):
+    return dt.fromtimestamp(created)
+
+_timestamp = attributes_data["created"].apply(get_date)
+topics_data = attributes_data.assign(timestamp = _timestamp)
+'''
