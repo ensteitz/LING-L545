db916d278a782f54d5147a9a29a9393a9dae3c6a
Rin Steitz <erin.steitz@gmail.com>
2024-04-30 22:25:08 -0400

    update

---
+++ 01_Encoding/README.md
@@ -0,0 +1,8 @@
+# Encoding Practical:
+
+## Instructions:
+In order to use this code, please run the file encoding.py through the command line:
+
+'''
+cat wiki.txt | python3 encoding.py
+'''
--- 01_Encoding/encoding.py
+++ 01_Encoding/encoding.py
@@ -2,7 +2,6 @@
 This is the first file that should be ran in this practical, then checker.py
 I made sure to explain my code, so it should be clear. If you have any questions, you can ask me.
 '''
-
 import sys
 import string
 
@@ -11,7 +10,7 @@ alphabet = set("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzÀÈÌÒÙÁ
 
 char_freq = {}
 
-def process_line(line, destination):
+def process_line(line):
     valid_line = False
 
     for char in line:
@@ -25,20 +24,16 @@ def process_line(line, destination):
 
     # Print the line ONLY IF it contains valid characters
     if valid_line:
-        destination.write(line)
+        sys.stdout.write(line)
 
-# Open the file in this file. Could have used it as sys.stdin, but I find this easier
-with open("wiki.txt", "r") as source, open("verified_alphabet_wiki.txt", "w") as destination:
-    for line in source:
-        process_line(line, destination)
+# Process lines from stdin
+for line in sys.stdin:
+    process_line(line)
 
 
 # This part took me forever!
 # Should be fixed now
-# Not only will the results be stored in wiki.valid, but it will also be sorted thanks to the string library
+# Sort and output the unknown characters to stdout
 unknown_chars = sorted(((freq, char) for char, freq in char_freq.items() if char not in alphabet and char not in string.punctuation), reverse=True)
-
-# Now export the buggers
-with open("wiki.valid", "w") as f_unknown_chars:
-    for freq, char in unknown_chars:
-        f_unknown_chars.write(f"{freq}\t{char}\n")
+for freq, char in unknown_chars:
+    sys.stderr.write(f"{freq}\t{char}\n")
